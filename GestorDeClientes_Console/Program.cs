f159c2ced0cf2fb24f3815be502e2fe7e2da5e9d
Thyago Santos <thyago@thyagosantorini.com>
2025-04-04 19:26:26 -0300

    Implementação do Menu do programa

--- GestorDeClientes_Console/Program.cs
+++ GestorDeClientes_Console/Program.cs
@@ -8,8 +8,35 @@ namespace GestorDeClientes_Console
 {
     internal class Program
     {
+
+        enum Menu { Adicionar =1, Remover, Listagem, Sair }
+
         static void Main(string[] args)
         {
+            bool opcaoSair = false;
+
+            while (!opcaoSair) 
+            {
+                Console.WriteLine("Sistema de Clientes - Seja Bem vindo(a)!");
+                Console.WriteLine("1 - Adicionar\n2 - Remover\n3 - Listagem\n4 - Sair");
+                int numeroOpcao = int.Parse(Console.ReadLine());
+                Menu opcao = (Menu)numeroOpcao;
+
+                switch (opcao)
+                {
+                    case Menu.Adicionar:
+                        break;
+                    case Menu.Remover:
+                        break;
+                    case Menu.Listagem:
+                        break;
+                    case Menu.Sair:
+                        opcaoSair = true;
+                        break;
+                }
+                Console.Clear();
+            }
+         
         }
     }
 }
